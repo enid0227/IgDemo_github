a990b754cdc4bdc32ff0a5e27bee566f0533b8bd
“enid0227” <enid0227@gmail.com>
2020-05-29 16:15:51 -0700

    Disply static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from Insta.views import HelloWorld
 # from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Insta/', include('Insta.urls'))
+     path('', HelloWorld.as_view(), name='helloworld')
 ]
